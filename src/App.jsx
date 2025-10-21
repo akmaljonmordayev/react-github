df89cac5d7ef315ed950c99df6a941dee6c6e4aa
akmaljonmordayev <akmaljonmordayev@gmail.com>
2025-10-21 14:14:01 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>TEAM WORK GITHUB</h1>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+        voluptas debitis veniam reiciendis inventore at explicabo optio autem
+        voluptates porro.
+      </h1>
+    </>
+  );
+}
+
+export default App;
